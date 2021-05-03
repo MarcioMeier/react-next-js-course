52af1f513111ed1b8018c14efca78a409b49d477
Márcio Meier <marciohmeier97@gmail.com>
2021-05-03 18:57:12 -0300

    chore: snapshot in external files

--- client/src/components/Main/test.tsx
+++ client/src/components/Main/test.tsx
@@ -20,31 +20,6 @@ describe('<Main />', () => {
   it('should render correctly', () => {
     const { container } = render(<Main />)
 
-    expect(container.firstChild).toMatchInlineSnapshot(`
-      <main
-        class="styles__Wrapper-sc-1cs6c8-0 bBVefU"
-      >
-        <img
-          alt="Imagem de um átomo e React Avançado escrito ao lado"
-          class="styles__Logo-sc-1cs6c8-1 eivRWX"
-          src="/img/logo.svg"
-        />
-        <h1
-          class="styles__Title-sc-1cs6c8-2 bQPDep"
-        >
-          React Avançado
-        </h1>
-        <h2
-          class="styles__Description-sc-1cs6c8-3 jApXu"
-        >
-          Typescript, ReactJS, NextJS e StyledComponents
-        </h2>
-        <img
-          alt="Um desenvolvedor de frente para uma tela com código"
-          class="styles__Illustration-sc-1cs6c8-4 cZHMdn"
-          src="/img/hero-illustration.svg"
-        />
-      </main>
-    `)
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
