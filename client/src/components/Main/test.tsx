63419aa074f0de165f151c969088843dd666e50a
Márcio Meier <marciohmeier97@gmail.com>
2021-05-03 18:49:55 -0300

    feat: main page

--- client/src/components/Main/test.tsx
+++ client/src/components/Main/test.tsx
@@ -11,14 +11,39 @@ describe('<Main />', () => {
     ).toBeInTheDocument()
   })
 
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
+
   it('should render correctly', () => {
     const { container } = render(<Main />)
 
     expect(container.firstChild).toMatchInlineSnapshot(`
-      <main>
-        <h1>
+      <main
+        class="styles__Wrapper-sc-1cs6c8-0 bBVefU"
+      >
+        <img
+          alt="Imagem de um átomo e React Avançado escrito ao lado"
+          class="styles__Logo-sc-1cs6c8-1 eivRWX"
+          src="/img/logo.svg"
+        />
+        <h1
+          class="styles__Title-sc-1cs6c8-2 bQPDep"
+        >
           React Avançado
         </h1>
+        <h2
+          class="styles__Description-sc-1cs6c8-3 jApXu"
+        >
+          Typescript, ReactJS, NextJS e StyledComponents
+        </h2>
+        <img
+          alt="Um desenvolvedor de frente para uma tela com código"
+          class="styles__Illustration-sc-1cs6c8-4 cZHMdn"
+          src="/img/hero-illustration.svg"
+        />
       </main>
     `)
   })
